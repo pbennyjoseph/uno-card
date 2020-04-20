3ed182cf3f5441e6ff52d681fee3eeaa2f8bbd19
AbhiCake <abhishekmarrivagu1@gmail.com>
2020-04-20 23:32:22 +0530

    Added button functionalities

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,35 @@
 // main functions to interact with backend
+
+// variables start ------------
+var btnJoinGame = document.getElementById('JoinGame');
+var btnCreateGame = document.getElementById('CreateGame');
+
+// variables end---------------
+
+function checkName(){
+	var value = document.getElementById("name").value;
+    if (value === '') {
+      alert('Enter your name');
+    }
+}
+
+function checkCode(){
+	var value = document.getElementById("code").value;
+    if (value === '') {
+      alert('Enter the code');
+    }
+}
+
+
+// checks if name is entered and starts game else alerts
+btnCreateGame.addEventListener("click",function(){
+    checkName();
+});
+
+// checks if name and code is entered and starts game else alerts
+btnJoinGame.addEventListener("click",function(){
+    checkName();
+	checkCode();
+
+});
+
