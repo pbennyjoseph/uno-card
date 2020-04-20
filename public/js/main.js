d603c18e6e2d195933c861ceefedb96557c5f692
AbhiCake <abhishekmarrivagu1@gmail.com>
2020-04-20 23:39:28 +0530

    Improved button functionalities

--- public/js/main.js
+++ public/js/main.js
@@ -6,30 +6,45 @@ var btnCreateGame = document.getElementById('CreateGame');
 
 // variables end---------------
 
+
+// functions start ------------------
 function checkName(){
 	var value = document.getElementById("name").value;
     if (value === '') {
       alert('Enter your name');
+      return false;
     }
+    return true;
 }
 
 function checkCode(){
 	var value = document.getElementById("code").value;
     if (value === '') {
       alert('Enter the code');
+      return false;
     }
+    return true;
 }
 
 
 // checks if name is entered and starts game else alerts
 btnCreateGame.addEventListener("click",function(){
-    checkName();
+    if(checkName()) {
+    	// start game if true
+
+    }
 });
 
 // checks if name and code is entered and starts game else alerts
 btnJoinGame.addEventListener("click",function(){
-    checkName();
-	checkCode();
+    if(checkName() && checkCode()){
+    	// start game if true
 
+
+    }
 });
+
+
+
+// functions end-------------------------
 
