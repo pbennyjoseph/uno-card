c9719e60cc5faa86ae40f5017ee3817465f1fe9f
Benny Joseph <jbenny37@gmail.com>
2020-04-21 11:36:17 +0530

    created cards for creator

--- node/src/app.js
+++ node/src/app.js
@@ -7,11 +7,7 @@ const io = require('socket.io')(server);
 
 let rooms = 0;
 
-app.use(express.static('.'));
-
-app.get('/', (req, res) => {
-    res.send('Welcome to game server!')
-});
+app.use(express.static('../../public'));
 
 io.on('connection', (socket) => {
 
@@ -19,34 +15,42 @@ io.on('connection', (socket) => {
     socket.on('createGame', (data) => {
         socket.roomLength = data.length
         socket.join(`room-${++rooms}`);
-        socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
+        socket.emit('newGame', {
+            name: data.name,
+            room: `room-${rooms}`
+        });
+        console.log('Creating new game');
     });
 
     // Connect the Player 2 to the room he requested. Show error if room full.
     socket.on('joinGame', function (data) {
         var room = io.nsps['/'].adapter.rooms[data.room];
-        if (room && room.length === socket.roomLength) {
+        if (room && room.length <= 3) {
             socket.join(data.room);
             socket.broadcast.to(data.room).emit('player1', {});
-            socket.emit('player2', { name: data.name, room: data.room })
+            socket.emit('player2', {
+                name: data.name,
+                room: data.room
+            })
         } else {
-            socket.emit('err', { message: 'Sorry, The room is full!' });
+            socket.emit('err', {
+                message: 'Sorry, The room is full!'
+            });
         }
+
+        console.log('joining new game');
     });
 
     /**
-       * Handle the turn played by either player and notify the other.
-       */
+     * Handle the turn played by either player and notify the other.
+     */
     socket.on('playTurn', (data) => {
-        socket.broadcast.to(data.room).emit('turnPlayed', {
-            tile: data.tile,
-            room: data.room
-        });
+        socket.broadcast.to(data.room).emit('turnPlayed', data);
     });
 
     /**
-       * Notify the players about the victor.
-       */
+     * Notify the players about the victor.
+     */
     socket.on('gameEnded', (data) => {
         socket.broadcast.to(data.room).emit('gameEnd', data);
     });
--- public/js/duplicate.js
+++ public/js/duplicate.js
@@ -5,22 +5,23 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
     let player;
     let game;
 
+    // const socket = io.connect('http://tic-tac-toe-realtime.herokuapp.com'),
     const socket = io.connect('http://localhost:5000');
 
     class Player {
         constructor(name) {
             this.name = name;
             this.currentTurn = true;
-            this.playsArr = [];
-            this.createPlayerCards();
+            this.playsArr = createPlayerCards();
         }
-
-
-        createPlayerCards() {
-            for (var i = 0; i < 7; ++i) {
+        
+        createPlayerCards(){
+            var ret = []
+            for (var i = 0;i < 7; ++i){
                 var card = randCard();
-                this.playsArr.push(card);
+                ret.push(card);
             }
+            return ret;
         }
 
         get wins() {
@@ -47,14 +48,16 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
         }
     }
 
+    // roomId Id of the room in which the game is running on the server.
     class Game {
-        constructor(roomId, player) {
+        constructor(roomId) {
             this.roomId = roomId;
             this.table = [];
         }
 
+        // Create the Game table by attaching event listeners to the buttons.
         createGameTable() {
-            function cardClickHandler() {
+            function tileClickHandler() {
                 const row = parseInt(this.id.split('_')[1][0], 10);
                 const col = parseInt(this.id.split('_')[1][1], 10);
                 if (!player.getCurrentTurn() || !game) {
@@ -67,6 +70,7 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
                     return;
                 }
 
+                // Update board after your turn.
                 game.playTurn(this);
                 game.updateBoard(player.getPlayerType(), row, col, this.id);
 
@@ -75,62 +79,120 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
 
                 game.checkWinner();
             }
-            var initArray = player.getPlaysArr();
-            for (var i = 0; i < initArray.length; ++i) {
-                $("#mycards").append(`<button class="btn btn-primary mx-1 my-1" value="${initArray[i]}" id="${i}">${initArray[i]}</button>`)
-                $(`#${i}`).on('click', cardClickHandler);
+
+            for (let i = 0; i < 3; i++) {
+                this.board.push(['', '', '']);
+                for (let j = 0; j < 3; j++) {
+                    $(`#button_${i}${j}`).on('click', tileClickHandler);
+                }
             }
         }
-        displayCards(message) {
+        // Remove the menu from DOM, display the gameboard and greet the player.
+        displayBoard(message) {
             $('#userHello').html(message);
-            this.createGameTable();
+            this.createGameBoard();
         }
-
-        updateCards(cardElem) {
-            cardElem.remove();
+        /**
+         * Update game board UI
+         *
+         * @param {string} type Type of player(X or O)
+         * @param {int} row Row in which move was played
+         * @param {int} col Col in which move was played
+         * @param {string} tile Id of the the that was clicked
+         */
+        updateBoard(type, row, col, tile) {
+            $(`#${tile}`).text(type).prop('disabled', true);
+            this.board[row][col] = type;
+            this.moves++;
         }
 
         getRoomId() {
             return this.roomId;
         }
 
-        playTurn(cardElem) {
-            const clickedCard = cardElem.attr('value');
+        // Send an update to the opponent to update their UI's tile
+        playTurn(tile) {
+            const clickedTile = $(tile).attr('id');
 
+            // Emit an event to update other player that you've played your turn.
             socket.emit('playTurn', {
-                card: clickedCard,
+                tile: clickedTile,
                 room: this.getRoomId(),
             });
         }
-
+        /**
+         *
+         * To determine a win condition, each square is "tagged" from left
+         * to right, top to bottom, with successive powers of 2.  Each cell
+         * thus represents an individual bit in a 9-bit string, and a
+         * player's squares at any given time can be represented as a
+         * unique 9-bit value. A winner can thus be easily determined by
+         * checking whether the player's current 9 bits have covered any
+         * of the eight "three-in-a-row" combinations.
+         *
+         *     273                 84
+         *        \               /
+         *          1 |   2 |   4  = 7
+         *       -----+-----+-----
+         *          8 |  16 |  32  = 56
+         *       -----+-----+-----
+         *         64 | 128 | 256  = 448
+         *       =================
+         *         73   146   292
+         *
+         *  We have these numbers in the Player.wins array and for the current
+         *  player, we've stored this information in playsArr.
+         */
         checkWinner() {
-            if (player.wins()) {
-                game.announceWinner();
+            const currentPlayerPositions = player.getPlaysArr();
+
+            Player.wins.forEach((winningPosition) => {
+                if ((winningPosition & currentPlayerPositions) === winningPosition) {
+                    game.announceWinner();
+                }
+            });
+
+            const tieMessage = 'Game Tied :(';
+            if (this.checkTie()) {
+                socket.emit('gameEnded', {
+                    room: this.getRoomId(),
+                    message: tieMessage,
+                });
+                alert(tieMessage);
+                location.reload();
             }
         }
 
+        checkTie() {
+            return this.moves >= 9;
+        }
+
+        // Announce the winner if the current client has won. 
+        // Broadcast this on the room to let the opponent know.
         announceWinner() {
-            winMessage = `${player.getPlayerName()} wins!`;
+            const message = `${player.getPlayerName()} wins!`;
             socket.emit('gameEnded', {
                 room: this.getRoomId(),
-                message: winMessage,
+                message,
             });
-            alert(winMessage);
+            alert(message);
             location.reload();
         }
 
+        // End the game if the other player won.
         endGame(message) {
             alert(message);
             location.reload();
         }
     }
 
-    function randCard() {
+    function randCard(){
         var color = CARDS[Math.floor(Math.random() * CARDS.length)];
         var digit = digits[Math.floor(Math.random() * digits.length)];
         return color + digit;
     }
 
+    // Create a new game. Emit newGame event.
     $('#new').on('click', () => {
         const name = $('#name').val();
         if (!name) {
@@ -140,11 +202,12 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
         socket.emit('createGame', {
             name,
         });
-        player = new Player(name);
+        player = new Player(name, P1);
     });
 
+    // Join an existing game on the entered roomId. Emit the joinGame event.
     $('#join').on('click', () => {
-        const name = $('#name').val();
+        const name = $('#nameJoin').val();
         const roomID = $('#room').val();
         if (!name || !roomID) {
             alert('Please enter your name and game ID.');
@@ -154,18 +217,18 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
             name,
             room: roomID
         });
-        player = new Player(name);
+        player = new Player(name, P2);
     });
 
     // New Game created by current client. Update the UI and create new Game var.
     socket.on('newGame', (data) => {
         const message =
             `Hello, ${data.name}. Please ask your friend to enter Game ID: 
-        ${data.room}. Waiting for others..`;
+        ${data.room}. Waiting for player 2...`;
 
         // Create game for player 1
         game = new Game(data.room);
-        game.displayCards(message);
+        game.displayBoard(message);
     });
 
     /**
@@ -178,20 +241,29 @@ const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3',
         player.setCurrentTurn(true);
     });
 
+    /**
+     * Joined the game, so player is P2(O). 
+     * This event is received when P2 successfully joins the game room. 
+     */
     socket.on('player2', (data) => {
         const message = `Hello, ${data.name}`;
 
+        // Create game for player 2
         game = new Game(data.room);
         game.displayBoard(message);
         player.setCurrentTurn(false);
     });
 
+    /**
+     * Opponent played his turn. Update UI.
+     * Allow the current player to play now. 
+     */
     socket.on('turnPlayed', (data) => {
         const row = data.tile.split('_')[1][0];
         const col = data.tile.split('_')[1][1];
         const opponentType = player.getPlayerType() === P1 ? P2 : P1;
 
-        game.updateBoard(card);
+        game.updateBoard(opponentType, row, col, data.tile);
         player.setCurrentTurn(true);
     });
 
